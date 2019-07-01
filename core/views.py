de4c208b3c2c50bb34176e93170023df4c748e44
bennykoval <bethanykoval@gmail.com>
2019-07-01 18:56:52 -0400

    Work out habit view, connect templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from core.models import Habit, DailyRecord
+from django.views import generic
+# from django.views.generic import TemplateView
 # Create your views here.
+
+class HabitView(generic.ListView):
+    model = Habit
+    template_name = "index.html"
