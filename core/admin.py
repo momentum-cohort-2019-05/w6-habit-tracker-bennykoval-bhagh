61fb6a413fe3d776643aa426d8821ced981db29e
bhagh <haghb1@gmail.com>
2019-07-01 18:15:59 -0400

    Fix shit

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Habit, DailyRecord
 
 # Register your models here.
+
+admin.site.register(Habit)
+admin.site.register(DailyRecord)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+import datetime
 
 # Create your models here.
+
+class Habit(models.Model):
+    """Model representing a habit"""
+    goal = models.TextField(max_length=1000, help_text="Enter your daily goal here.")
+    quantity = models.PositiveIntegerField(default=1)
+    date = models.DateField(auto_now_add=True)  
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["-date"]
+    
+    def __str__(self):
+        return self.goal
+
+class DailyRecord(models.Model):
+    description = models.TextField(max_length=500, help_text="Enter comment about blog here.")
+    date = models.DateField(auto_now_add=True)
+    habit = models.ForeignKey(to=Habit, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ["-date"]
+    
+    def __str__(self):
+        return self.description
+    
+    
